d6cba9cc6aeb3749ddf85c2f207bb4942bba307a
Thomas Gak Deluen <tgdn@hotmail.fr>
2011-06-17 09:47:25 +0200

    Edited setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,18 @@
 import os
 import uploader.settings as settings
 
-os.chdir("%/../" % settings.MEDIA_ROOT)
+os.chdir("%s/../" % settings.MEDIA_ROOT)
 
-os.mkdir("db/")
+try:
+  os.mkdir("db/")
+except OSError:
+  pass
+
 os.system("python manage.py syncdb")
 os.chdir(settings.MEDIA_URL)
 
 try:
-  os.mkdir("uploads/" % settings.MEDIA_ROOT)
+  os.mkdir("uploads/")
 except OSError:
   print("\n\n============= Everything is installed and up-to-date =============\n")
   print("\tRun server with commands: python manage.py runserver [[port]]\n")
